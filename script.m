728c463a6cc9754a38a1d798bdb8a7e1619e19dd
Akhil Jain <akhiljain93>
2014-10-24 08:57:49 +0530

    Extended filter to all channels
    Generalised the code a bit, ignore asv files from now on

--- script.m
+++ script.m
@@ -1,31 +1,30 @@
 load('Patient_8_ictal_segment_1.mat');
 datat = data';
 down = downsample(datat,20); % downsampled to 250Hz
-input = down(:,1) %applying band pass filter on electrode 1 values
+input = zeros(size(down));
+
 % ############## LOW PASS FILTER ################
 % [b,a] = cheby2(6,40,13/125);   125 is half of sampling frequency
-% fvtool(b,a); Visualize filter
-% dataOut = filter(b,a,input);
-% figure;
-% [pxx,f] = periodogram(dataOut,[],[],250); %plot of pwoer without filter
-% plot(f,10*log10(pxx))
-% [pxx1,f1] = periodogram(input,[],[],250);
-% figure;
-% plot(f1,10*log10(pxx1)); %plot of pwoer without filter
-
+% fvtool(b,a); %Visualize filter
+% legend(fvt,'lowpass','designfilt')
 
 % ############## BAND PASS FILTER ################
 [A,B,C,D] = cheby2(10,40,[0.5 13]/125); % 125 is half of sampling frequency
 sos = ss2sos(A,B,C,D);
-% % Visualize filter
-% fvt = fvtool(sos,d,'Fs',1500);
+% fvt = fvtool(sos,d,'Fs',1500); %Visualize filter
 % legend(fvt,'cheby2','designfilt')
 
-banddata = sosfilt(sos,input);
-figure;
-[pxx,f] = periodogram(banddata,[],[],250); %plot of pwoer without filter
-plot(f,10*log10(pxx))
-[pxx1,f1] = periodogram(input,[],[],250);
-figure;
-plot(f1,10*log10(pxx1)); %plot of pwoer without filter
+for i=1:size(datat,2)
+   input = down(:,i); %applying band pass filter on electrode 1 values
+   % dataOut = filter(b,a,input); %lowpass
+   dataout = sosfilt(sos,input(:,i)); %bandpass
+   
+end
 
+% %Visualizing the power spectra
+% figure;
+% [pxx,f] = periodogram(dataout,[],[],250); %plot of power with filter
+% plot(f,10*log10(pxx))
+% [pxx1,f1] = periodogram(input,[],[],250);
+% figure;
+% plot(f1,10*log10(pxx1)); %plot of power without filter
