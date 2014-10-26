ee03226c6e52b01244c4221c20514d06563f3d29
Mayank Gupta <gptmayank51@gmail.com>
2014-10-26 11:21:13 +0530

    Tiny changes by mayank

--- find_params.m
+++ find_params.m
@@ -1,7 +1,9 @@
 function [params] = find_params(t,f)
     %params(i,1) -> peak frequency
+    %params(i,4) -> peak power
     %params(i,2) -> bandwidth
     %params(i,3) -> line length
+    %params(i,5) -> Total Power
     params = zeros(size(t,2),21);
     for i=1:size(t,2) %time domain features
         for j=1:length(t)-1
@@ -9,13 +11,27 @@ function [params] = find_params(t,f)
         end
     end
     for i=1:size(f,2) % frequency domain features
-        psd = periodogram(t(:,i),rectwin(length(t)),length(t),length(t));
-        mini = Inf;
-        maxi = -Inf;
-        for j=1:length(f)
-            mini = min(mini,f(j,i));
-            maxi = min(maxi,f(j,i));
-        end
-        params(i,1) = maxi;
-        params(i,2) = maxi-mini;
+        %hold on;
+        pxx = periodogram(t(:,i),[],512); %512 point fft LINK - http://www.mathworks.in/help/signal/ref/periodogram.html#btt5utg
+        % pxx is  periodogram power spectral density (PSD) estimate
+        %plot(10*log10(psd));
+        [maxVal, index] = max(pxx);
+        temp = abs(pxx - (maxVal/2));
+        [Val,Ind]= sort(temp);
+        params(i,1) = index;
+        params(i,4) = maxVal;
+        params(i,2) = abs(Ind(1) - Ind(2));
+        % Spectral Edge Frequency - For range 2-20Hz.
+        pband = bandpower(pxx,F,[2 20],'psd');  %LINK  - http://www.mathworks.in/help/signal/ref/bandpower.html#btrddkn-4
+        params(i,5) = pband;
+        
+%         mini = Inf;
+%         maxi = -Inf;
+%         for j=1:length(f)
+%             mini = min(mini,f(j,i));
+%             maxi = min(maxi,f(j,i));
+%         end
+        %params(i,1) = maxi;
+        %params(i,2) = maxi-mini;
     end
+    
