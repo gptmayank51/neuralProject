820b5dbe86bf6554acfadbfde1cc015a76ea6d5f
Akhil Jain <akhil.jain93@gmail.com>
2014-11-27 19:49:34 +0530

    include changes done by mayank for optimal printing

--- runMedian.m
+++ runMedian.m
@@ -15,7 +15,14 @@
 % data_generatorVal
 % load('../dataValOutput/input.mat');
 % %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
+clear;
+delete('myTextLogMedian.txt');
+diary('myTextLogMedian.txt');
+disp('=====================================');
+disp('=====================================');
+fprintf('New session started');
+disp('=====================================');
+disp('=====================================');
 testSet = -1;
 valSet = -1;
 countP = 8;
@@ -23,8 +30,8 @@ accu = zeros(1:countP);
 precision = zeros(1:countP);
 recall = zeros(1:countP);
 for opt = 1:countP
-    valSet = mod(i+5,countP)+1;
-    testSet = mod(i+6,countP)+1;
+    valSet = mod(opt+5,countP)+1;
+    testSet = mod(opt+6,countP)+1;
     disp('=====================================');
     fprintf('Generating X and Y Matrices for Training, Iteration no %d\n',opt);
     disp('=====================================');
@@ -54,47 +61,58 @@ for opt = 1:countP
     X = (X - (ones(size(X,1),1)*mean(X)))./(ones(size(X,1),1)*std(X)); %normalised features
     %coarse grained search for c and gamma
     addpath('C:\Users\seizure-detection\Desktop\nn\libsvm-3.20\libsvm-3.20\matlab');
-    acc = zeros(21,29);
-    for i=-5:15
+    acc = zeros(16,19);
+    for i=-5:10
         c = 2^i;
-        for j = -25:3
+        for j = -15:3
             gamma = 2^j;
             options = sprintf('-q -s 0 -t 2 -c %f -g %f', c, gamma);
-            gaus = svmtrain(class,X,options);
-            [~,acc(i+6,j+26),~] = svmpredict(class,X,gaus);
-        end 
+            gaus = svmtrain(trainClass,trainX,options);
+            [~,a,~] = svmpredict(class,X,gaus);
+            acc(i+6,j+16) = a(1);
+        end
+        disp('=====================================');
+        fprintf('Coarse grained search for gamma - Iteration over %d\n',i);
+        disp('=====================================');
     end
     temp = find(acc == max(max(acc)));
     median = temp(floor(length(temp)/2));
-    [gammamax,cmax] = quorem(median,sym(21));
+    [gammamax,cmax] = quorem(median,sym(16));
     cmax = cmax - 6;
-    gammamax = gammamax - 26;
+    gammamax = gammamax - 16;
     disp('=====================================');
     disp('Fine grained search for gamma');
     disp('=====================================');
     %fine grained search
-    crange = cmax-0.5:0.1:cmax+0.5;
-    gammarange = gammamax-0.5:0.1:gammamax+0.5;
-    acc = zeros(11,11);
+    crange = cmax-0.5:0.2:cmax+0.5;
+    gammarange = gammamax-0.5:0.2:gammamax+0.5;
+    acc = zeros(6,6);
     for i=1:length(crange)
         c = 2^crange(i);
         for j = 1:length(gammarange)
             gamma = 2^gammarange(j);
             options = sprintf('-q -s 0 -t 2 -c %f -g %f', c, gamma);
-            gaus = svmtrain(class,X,options);
-            [~,acc(i,j),~] = svmpredict(class,X,gaus);
-        end 
+            gaus = svmtrain(trainClass,trainX,options);
+            [~,a,~] = svmpredict(class,X,gaus);
+            acc(i,j) = a(1);
+        end
+        disp('=====================================');
+        fprintf('Fine grained search for gamma - Iteration over %d\n',i);
+        disp('=====================================');
     end
     temp = find(acc == max(max(acc)));
     median = temp(floor(length(temp)/2));
     [gammamax,cmax] = quorem(median,sym(11));
     gamma = 2^gammarange(gammamax);
     c = 2^crange(cmax);
+    disp('=====================================');
+    fprintf('Optimal Gamma is %d, C is %d, Training Starting now!!\n',gamma,c);
+    disp('=====================================');
     
     %%%%%%%%%%%%%%%%%%%%%%%%%%% TRAINING %%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
     options = sprintf('-s 0 -t 2 -c %f -g %f', c, gamma);
-    gaus = svmtrain(trainClass,trainX,options);
+    gaus = svmtrain([trainClass;class],[trainX;X],options);
     save ('model.mat','gaus');
 
     %%%%%%%%%%%%%%%%%%%%%%%%%%% TESTING %%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -115,11 +133,11 @@ for opt = 1:countP
     for i=1:length(testClass)
        if testClass(i)==1 && prediction(i)==1 %TP
            counts(1) = counts(1) + 1;
-       elseif testClass(i)==1 && prediction(i)==0 %FN
+       elseif testClass(i)==1 && prediction(i)==-1 %FN
            counts(2) = counts(2) + 1;
-       elseif testClass(i)==0 && prediction(i)==1 %FP
+       elseif testClass(i)==-1 && prediction(i)==1 %FP
            counts(3) = counts(3) + 1;
-       elseif testClass(i)==0 && prediction(i)==0 %TN
+       elseif testClass(i)==-1 && prediction(i)==-1 %TN
            counts(4) = counts(4) + 1;
        end
     end
@@ -128,8 +146,11 @@ for opt = 1:countP
     fprintf('Accuracy reported is %d\n',accu(opt));
     fprintf('Precision reported is %d\n',precision(opt));
     fprintf('Recall reported is %d\n',recall(opt));
+    disp('Count is');
+    disp(counts);
 end
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 fprintf('Average accuracy reported is %d\n',mean(acc));
 fprintf('Average precision reported is %d\n',mean(precision));
 fprintf('Average recall reported is %d\n',mean(recall));
+diary('off');
